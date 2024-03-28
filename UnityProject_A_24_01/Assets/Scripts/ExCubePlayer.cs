b73eca2d596bb5bd8ae9713a994d0b4c0652b0fe
shingugitvr000 <shingugitvr000@gmail.com>
2024-03-28 15:54:43 +0900

    [Feat] 물리 충돌 까지 모델링 임포트

--- UnityProject_A_24_01/Assets/Scripts/ExCubePlayer.cs
+++ UnityProject_A_24_01/Assets/Scripts/ExCubePlayer.cs
@@ -8,23 +8,36 @@ public class ExCubePlayer : MonoBehaviour
     public Text TextUI = null;              //텍스트 UI
     public int Count = 0;                   //마우스 클릭 카운터
     public int Power = 100;            //물리 힘 수치
+    
+    public int Point = 0;                   //점수 수치
+    public float checkTime = 0.0f;          //시간 체크 표시
+
     public Rigidbody m_Rigidbody;           //오브젝트의 강체 
 
     // Update is called once per frame
     void Update()
-    {        
-        if (Input.GetKeyDown(KeyCode.Space)) //스페이스를 누를 때 
+    {
+
+        checkTime += Time.deltaTime;            //시간을 누적해서 쌓는다. checkTime -> 0초 , 1초 , 0초, 1초
+        if (checkTime >= 1.0f)                   //1초마다 어떤 행동을 한다. 
         {
-            Count += 1;                                 //마우스가 클릭되었을때 Count를 1씩 올린다.
-            TextUI.text = Count.ToString();                 //UI 갱신
+            Point += 1;                         //1초마다 점수 1점을 올린다. 
+            checkTime = 0.0f;                   //시간을 초기화 한다.
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space)) //스페이스를 누를 때 
+        {           
             Power = Random.Range(100, 200);                 // 100 ~ 200 사이의 값의 힘을 준다
             m_Rigidbody.AddForce(transform.up * Power);     //Y축으로 설정한 힘을 준다.            
         }
 
-        if(gameObject.transform.position.y >= 2 || gameObject.transform.position.y <= -2)
-        {   //오브젝트의 y값이 -2 이하이거나 2이상일경우 조건문
-            TextUI.text = "실패";
-            Count = 0;                                  //실패시 카운터 초기화
-        }
+        TextUI.text = Point.ToString();                     //UI에 점수 표시를 한다. 
+    }
+
+    void OnCollisionEnter(Collision collision)          //충돌이 되었을 때
+    {
+        //Debug.Log(collision.gameObject.name);
+        Point = 0;
+        gameObject.transform.position = Vector3.zero;   //플레이어를 원점으로 이동 시킨다. 
     }
 }
